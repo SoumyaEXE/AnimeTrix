d63bd9421c0b1ffac2af035e70fc57df5e860c52
Soumyadeep Dey <95275364+SoumyaEXE@users.noreply.github.com>
2023-05-15 10:37:14 +0530

    Update Footer.jsx

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -18,9 +18,14 @@ function Footer() {
         <span className="white">Anime</span>Trix
       </h1>
       <p className="footer-about">
-AnimeTrix is not affiliated with or endorsed by any of the anime studios behind the creation of the anime presented on this site.A heartfelt thank you to the creator(🔥Shiva Bhattacharjee & Dark Knight❤) for bringing this stunning website to life. I (🔥Soumyadeep Dey❤) am committed to enhancing its beauty and user experience even further.  
-     This website is only an user interface presenting/linking various self-hosted files across the internet by other third-party providers for easy access. AnimeTrix never downloads the video from any source provider, link will be returned from the response hence it is completely not subjected to 
-         DMCA compliant.
+AnimeTrix is not affiliated with or endorsed by any of the anime studios
+        behind the creation of the anime presented on this site.A heartfelt thank you to the creators (🔥Shiva Bhattacharjee & Dark-Knight❤) for bringing this stunning website to life. I (🔥Soumyadeep Dey❤) am committed to enhancing its beauty and user experience even further. 
+This website is
+        only an user interface presenting/linking various self-hosted files
+        across the internet by other third-party providers for easy access.
+        AnimeTrix never downloads the video from any source provider, link will
+        be returned from the response hence it is completely not subjected to
+        DMCA compliant.
       </p>
       <div className="footer-social">
         <button onClick={() => instagramUrl()}>
