6807ccfdadc5db423d59237962a20b265e3dd4d0
Soumyadeep Dey <95275364+SoumyaEXE@users.noreply.github.com>
2023-05-15 11:06:43 +0530

    Update Footer.jsx

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -34,8 +34,8 @@ This website is
         <button onClick={() => githubUrl()}>
           <GitHubIcon />
         </button>
-      </div> <h6>Ⓒ 2023-2024<a href="https://github.com/SoumyaEXE">Soumyadeep Dey</a></h6>
-    </div>
+      </div>
+ <h6>Ⓒ 2023-2024 Soumyadeep Dey</h6>
   );
 }
 export default Footer;
