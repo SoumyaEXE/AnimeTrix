94293abbcd7f5a9de896ca4b5ef48102f587f6a3
Soumyadeep Dey <95275364+SoumyaEXE@users.noreply.github.com>
2023-05-22 07:27:23 +0530

    Update Footer.jsx

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -5,26 +5,27 @@ import GitHubIcon from "@mui/icons-material/GitHub";
 function Footer() {
 
   const githubUrl = () => {
-    window.open('https://github.com/ShivaBhattacharjee/AnimeTrix')
+    window.open('https://github.com/SoumyaEXE/AnimeZen')
   }
 
   const instagramUrl = () => {
-    window.open('https://www.instagram.com/animetrix.200/')
+    window.open('https://instagram.com/s0umy4_xD')
   }
   const discordUrl = () => {
-    window.open('https://discord.gg/t7xSMNr7zN')
+    window.open('https://discord.gg')
   }
   return (
     <div className="footer">
       <h1>
-        <span className="white">Anime</span>Trix
+        <span className="white">Anime</span>Zen
       </h1>
       <p className="footer-about">
-        AnimeTrix is not affiliated with or endorsed by any of the anime studios
-        behind the creation of the anime presented on this site. This website is
+        AnimeZen is not affiliated with or endorsed by any of the anime studios
+        behind the creation of the anime presented on this site.A heartfelt thank you to the creators (🔥Shiva Bhattacharjee & Dark-Knight❤) for bringing this stunning website to life. I (🔥Soumyadeep Dey❤) am committed to enhancing its beauty and user experience even further. 
+This website is
         only an user interface presenting/linking various self-hosted files
         across the internet by other third-party providers for easy access.
-        AnimeTrix never downloads the video from any source provider, link will
+        AnimeZen never downloads the video from any source provider, link will
         be returned from the response hence it is completely not subjected to
         DMCA compliant.
       </p>
@@ -37,6 +38,7 @@ function Footer() {
           <GitHubIcon />
         </button>
       </div>
+ <p className="footer"> <h6>Ⓒ 2023-2024 Soumyadeep Dey</h6> </p>
     </div>
   );
 }
